33ded166322ee24a7370e7eb7aee86a474d254e6
Danidev819 <elprodaniel13@gmail.com>
2024-07-28 12:24:35 +0200

    Progress Bar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ProgressBar from "./components/ProgressBar";
+
+function App() {
+	const [progress, setProgress] = useState(0);
+
+	const updateProgress = () => {
+		setProgress((prevProgress) => {
+			if (prevProgress >= 100) {
+				return 0;
+			}
+			return prevProgress + 10;
+		});
+	};
+
+	return (
+		<div>
+			<ProgressBar progress={progress} />
+			<button onClick={updateProgress}>Increase Progress</button>
+		</div>
+	);
+}
+
+export default App;
